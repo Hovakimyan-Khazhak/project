a0a1f0d618282d648377e078036f2769529b9450
Khazhak Hovakimyan <Khhovakimyan1986@gmail.com>
2025-03-26 15:56:34 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// const numberOfFilms = +prompt("Skolko filmov vi uje posmotreli?", "")
+//
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     private: false
+// }
+//
+// const a = prompt("Odin is poslednix prosmotrennix filmov", ""),
+//       b = prompt("Na skolko ocenite ego", ""),
+//       c = prompt("Odin is poslednix prosmotrennix filmov", ""),
+//       d = prompt("Na skolko ocenite ego", "");
+//
+//
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+//
+// console.log(personalMovieDB)
+"use strict";
+

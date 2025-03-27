02dfa8154bba220f4483475be25cd6c4717ac0ea
Khazhak Hovakimyan <Khhovakimyan1986@gmail.com>
2025-03-27 14:12:15 +0400

    new file file

--- script.js
+++ script.js
@@ -20,18 +20,49 @@
 // console.log(personalMovieDB)
 "use strict";
 
-const hamburger =2;
-const fries = 1;
+// const hamburger =2;
+// const fries = 1;
+//
+// if(hamburger && fries){
+//     console.log("ya cit")
+// }
+//
+// console.log((hamburger && fries))
 
-if(hamburger && fries){
-    console.log("ya cit")
-}
-
-console.log((hamburger && fries))
-
+// console.log( NaN || 2 || undefined );
+//
+//
+// console.log( NaN && 2 && undefined );
 
 
+// console.log( 1 && 2 && 3 );
+//
+//
+// console.log( !1 && 2 || !3 );
+//
+//
+// console.log( 25 || null && !3 );
+//
+//
+// console.log( NaN || null || !3 || undefined || 5);
+//
+//
+// console.log( NaN || null && !3 && undefined || 5);
+//
+//
+// console.log( 5 === 5 && 3 > 1 || 5);
 
+
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+//
+//
+// if (hamburger === 3 && cola || fries === 3 && nuggets) {
+//     console.log('Done!')
+// }
 
 
 
